e538696b8c0786a78d15c5d15fe0f0ebce60f5df
FayneBouncing <139232456+FayneBouncing@users.noreply.github.com>
2023-12-08 04:59:38 +0000

    buttons dont work

---
+++ login.js
@@ -0,0 +1,14 @@
+let username = [];
+let password = [];
+let createbtn = document.getElementById("clbtn");
+function createAccount() {
+    console.log(document.getElementbyId(username).value);
+    for (let i = 0; i < username.length; i++) {
+        if (getElementById(username).value == username[i]) {
+            
+        }
+    }
+    for (let i = 0; i < password.length; i++) {
+    }
+}
+createbtn.addEventListener("click",createAccount());
