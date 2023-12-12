586f7c2d84e00763fa5ae27c6fcefa352e9107f8
FayneBouncing <139232456+FayneBouncing@users.noreply.github.com>
2023-12-12 17:35:23 +0000

    BUTTONS

--- login.js
+++ login.js
@@ -1,7 +1,11 @@
 const LoginBtn = document.getElementById("Lgbtn");
+const CreateBtn = document.getElementById("Cabtn");
 const Username = document.getElementById("User").value;
 let existingLogin = false;
 const list = 'login.txt';
 LoginBtn.addEventListener("click", function Login() {
- console.log(Username);
+ console.log("Login button clicked");
+});
+CreateBtn.addEventListener("click", function Create() {
+    console.log("Create button clicked");
 });
