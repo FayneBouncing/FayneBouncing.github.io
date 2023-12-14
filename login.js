56112e83eb1c47b5b28b6a8ea092be2aa41803a1
FayneBouncing <139232456+FayneBouncing@users.noreply.github.com>
2023-12-14 19:06:24 +0000

    Placeholder

--- login.js
+++ login.js
@@ -3,8 +3,9 @@ const CreateBtn = document.getElementById("Cabtn");
 const Username = document.getElementById("User").value;
 let existingLogin = false;
 const list = 'login.txt';
+const fs = require('fs')
 LoginBtn.addEventListener("click", function Login() {
- console.log("Login button clicked");
+    list.writeFile(list,"Sup");
 });
 CreateBtn.addEventListener("click", function Create() {
     console.log("Create button clicked");
