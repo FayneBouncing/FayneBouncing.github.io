20081309de69020b6da94c0d38367cff09b3007c
FayneBouncing <154368533+FayneBouncing@users.noreply.github.com>
2023-12-23 06:48:38 +0000

    top nav color change

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 const LoginBtn = document.getElementById("Lgbtn");
 const CreateBtn = document.getElementById("Cabtn");
-const Username = document.getElementById("User").value;
-const Password = document.getElementById("Pass").value;
+let Username = document.getElementById("User").value;
+let Password = document.getElementById("Pass").value;
 let existingLogin = false;
 const list = 'login.txt';
 LoginBtn.addEventListener("click", function Login() {
-    console.log(Username);
+    console.log("Login button clicked");
 });
 CreateBtn.addEventListener("click", function Create() {
     console.log("Create button clicked");
