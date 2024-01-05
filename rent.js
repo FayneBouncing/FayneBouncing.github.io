3467ab20f31085e6c0127d41f385facec112600d
FayneBouncing <154368533+FayneBouncing@users.noreply.github.com>
2024-01-05 06:14:48 +0000

    field Values

--- rent.js
+++ rent.js
@@ -1,5 +1,14 @@
 const RentBtn = document.getElementById("rent");
 let Name = document.getElementById("name");
+let PhoneNumber = document.getElementById("phone");
+let Email = document.getElementById("email");
+let Adress = document.getElementById("adress");
+let Date = document.getElementById("date");
+let Time = document.getElementById("time");
 RentBtn.addEventListener("click", function Rent() {
     console.log(Name.value);
+    console.log(PhoneNumber.value);
+    console.log(Email.value);
+    console.log(Adress.value);
+    console.log(Date.value + " @" + Time.value);
 });
