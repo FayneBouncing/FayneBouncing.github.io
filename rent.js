d092eaeca7e163c4feaadf759d80278d4aec5c40
FayneBouncing <154368533+FayneBouncing@users.noreply.github.com>
2024-01-06 23:42:42 +0000

    remove when submit

--- rent.js
+++ rent.js
@@ -1,14 +1,25 @@
 const RentBtn = document.getElementById("rent");
+let NameLabel = document.getElementById("fname");
 let Name = document.getElementById("name");
+let Pnumber = document.getElementById("Pnumber");
 let PhoneNumber = document.getElementById("phone");
+let EmailLabel = document.getElementById("EA");
 let Email = document.getElementById("email");
+let AddressLabel = document.getElementById("address");
 let Adress = document.getElementById("adress");
+let DateLabel = document.getElementById("Date");
 let Date = document.getElementById("date");
+let TimeLabel = document.getElementById("Time");
 let Time = document.getElementById("time");
+let BH = document.getElementById("BH");
 let BounceHouse = document.getElementById("bounceHouse");
+let PS = document.getElementById("PS");
 let PurpleSlide = document.getElementById("purpleSlide");
+let RS = document.getElementById("RS");
 let RainbowSlide = document.getElementById("rainbowSlide");
+let SS = document.getElementById("SS");
 let SlipSlide = document.getElementById("slipSlide");
+let VB = document.getElementById("VB");
 let Volleyball = document.getElementById("volleyball");
 RentBtn.addEventListener("click", function Rent() {
     console.clear();
@@ -32,5 +43,26 @@ RentBtn.addEventListener("click", function Rent() {
     if (Volleyball.checked) {
         console.log("Volleyball");
     }
+    NameLabel.style.display = "none";
     Name.style.display = "none";
+    Pnumber.style.display = "none";
+    PhoneNumber.style.display = "none";
+    EmailLabel.style.display = "none";
+    Email.style.display = "none";
+    AddressLabel.style.display = "none";
+    Adress.style.display = "none";
+    DateLabel.style.display = "none";
+    Date.style.display = "none";
+    TimeLabel.style.display = "none";
+    Time.style.display = "none";
+    BH.style.display = "none";
+    BounceHouse.style.display = "none";
+    PS.style.display = "none";
+    PurpleSlide.style.display = "none";
+    RS.style.display = "none";
+    RainbowSlide.style.display = "none";
+    SS.style.display = "none";
+    SlipSlide.style.display = "none";
+    VB.style.display = "none";
+    Volleyball.style.display = "none";
 });
